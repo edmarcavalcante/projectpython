390bb9636c756c7f29e866e94ada0ab3bb90ee49
edmarcavalcante <46653761+edmarcavalcante@users.noreply.github.com>
2020-01-09 07:21:22 -0300

    Add files via upload

---
+++ 1_meu_programa.py
@@ -0,0 +1,19 @@
+nome = str(input("Qual o seu nome? "))
+
+if nome == "Edmar":
+	print("Olá Edmar, seja bem bindo nessa nova empreitada!")
+else:
+	print("Olá, {}, você é novo aqui!!".format(nome))
+
+#o comando \n insere uma quebra de linha, já o \t acrecenta uma tabulação. 
+print ("Esse é o início de uma nova e encantadora jornada!\n\n\tVocê se supreenderá com o poder da programação.\n\nPor meio dessa dessa ferramenta,você poderá desenvolver projetos incríveis.")
+
+
+cpf = str(input('"Qual o seu CPF? '))
+cpf=cpf.strip() #a função strip retirar o espaço em branco existente antes da string
+print(cpf)
+
+
+#  princípios do python
+#import this
+
